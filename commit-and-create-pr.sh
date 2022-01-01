8f4ad6824ee9bedcc8ebfffbae6b772e1f96646f
Ichinose Shogo <shogo82148@gmail.com>
2022-01-01 20:43:10 +0900

    make temporary directory

--- commit-and-create-pr.sh
+++ commit-and-create-pr.sh
@@ -3,25 +3,32 @@
 set -ue
 set -o pipefail
 
+TMPDIR=$(mktemp -d)
+trap 'rm -rf "$TMPDIR"' EXIT
+
 git add .
 
 # addtions
 git diff -z --name-only --cached --no-renames --diff-filter=d | \
-    xargs -0 -n1 sh -c "git show \":0:\$0\" | jq --arg path \"\$0\" --raw-input --slurp --compact-output '{ path: \$path, contents: @base64 }'" > .additions.txt
+    xargs -0 -n1 sh -c \
+    "git show \":0:\$0\" | jq --arg path \"\$0\" --raw-input --slurp --compact-output '{ path: \$path, contents: @base64 }'" \
+    > "$TMPDIR/additions.txt"
 
 # deletions
-git diff -z --name-only --cached --no-renames --diff-filter=D | jq --raw-input --slurp 'split("\u0000")' > .deletions.txt
+git diff -z --name-only --cached --no-renames --diff-filter=D | \
+    jq --raw-input --slurp 'split("\u0000")' \
+    > "$TMPDIR.deletions.txt"
 
 jq --null-input \
-    --slurpfile additions .additions.txt \
-    --slurpfile deletions .deletions.txt \
+    --slurpfile additions "$TMPDIR/additions.txt" \
+    --slurpfile deletions "$TMPDIR.deletions.txt" \
     --arg expectedHeadOid "$(git rev-parse HEAD)" \
     --arg query 'mutation ($input: CreateCommitOnBranchInput!) {
         createCommitOnBranch(input: $input) {
             commit { url }
         }
     }' \
-    --arg message 'test of createCommitOnBranch' \
+    --arg message 'make temporary directory' \
     '{
         query: $query,
         variables: {
@@ -41,4 +48,4 @@ jq --null-input \
             }
         }
     }' | \
-    gh api graphql --input -
+    gh api graphql --input - --jq '.data.createCommitOnBranch.commit.url'
